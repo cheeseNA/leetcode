020978b11b5c19532f2cee837978a45ca6a59be8
cheeseNA <kurocat2000@gmail.com>
2024-03-22 12:21:13 +0900

    Add step3.py

---
+++ 141. Linked List Cycle/step3.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast_cursor = head
+        slow_cursor = head
+        while fast_cursor and fast_cursor.next:
+            fast_cursor = fast_cursor.next.next
+            slow_cursor = slow_cursor.next
+            if fast_cursor == slow_cursor:
+                return True
+        return False
